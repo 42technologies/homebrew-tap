6f9f7870ffe071874ec7463c353975a05f5ab0b8
Nick Porter <nick@42debut.com>
2025-04-29 22:55:54 -0700

    apache-spark: add version 3.1.3, 3.3.2, 3.5.3

---
+++ Formula/apache-spark@3.3.2.rb
@@ -0,0 +1,24 @@
+class ApacheSparkAT332 < Formula
+  desc "Engine for large-scale data processing"
+  homepage "https://spark.apache.org/"
+  url "https://archive.apache.org/dist/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz"
+  version "3.3.2"
+
+  depends_on "openjdk@17"
+
+  def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+
+    rm_f Dir["bin/*.cmd"]
+    libexec.install Dir["*"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix)
+  end
+
+  test do
+    assert_match "Long = 1000",
+      pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1",
+                  "sc.parallelize(1 to 1000).count()")
+  end
+end
